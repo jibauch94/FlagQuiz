4b49583f3987596ecf0f620fcbe33529210ed9cd
jibauch94 <jib090994@gmail.com>
2017-05-05 11:53:34 +0200

    commit 7
    Cant run,, gui isnt good wrong image on phone

--- app/src/main/java/com/example/jibba_000/flagquiz/MainActivity.java
+++ app/src/main/java/com/example/jibba_000/flagquiz/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.jibba_000.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +15,43 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    // keys for reading data from SharedPreferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = " pref_regionsToInclude";
 
+    private boolean phoneDevice = true; //used to force portrait mode
+    private boolean preferencesChanged = true; // did preferences change?
+
+//configure the MainActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //set default values in the apps SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        //register listener for sharedpreferences changes
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        // determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // if device is a tablet, set phoneDevice to false
+        if(screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE||screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+        phoneDevice = false; // not a phone-sized device
+
+        //if running on phone-sized device, allow only portrait orientation
+        if(phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,25 +63,80 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // called after onCreate completes execution
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        if (preferencesChanged){
+            //now that the default preferences have been set,
+            //initialize MainActivityFragment and start quiz
+            MainActivityFragment quizFragment = (MainActivityFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+        }
+    }
+
+    //show menu if app is running on a phone or portrait-oriented tablet
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        //get the device current orientation
+        int orientation = getResources().getConfiguration().orientation;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //display the app menu only in portrait
+        if(orientation == Configuration.ORIENTATION_PORTRAIT){
+            //inflate the menu
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+        else
+            return false;
     }
+
+    //display the SettingsActivity when running on a phone
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        return super.onOptionsItemSelected(item);
+
+    }
+    //listener for changes to the apps sharedPreferences
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener = new SharedPreferences.OnSharedPreferenceChangeListener(){
+        //called when user changes the apps preferences
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key){
+            preferencesChanged = true; //user changed app settings
+
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+            if(key.equals(CHOICES)){
+                quizFragment.updateGuessRows(sharedPreferences);
+                quizFragment.resetQuiz();
+            }
+            else if (key.equals(REGIONS)){
+                Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+
+                if(regions != null && regions.size() > 0){
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.resetQuiz();
+                }
+                else {
+                    //must select ine region - set North America as default
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+            }
+            Toast.makeText(MainActivity.this, R.string.restarting_quiz, Toast.LENGTH_SHORT).show();
+        }
+
+    };
 }
